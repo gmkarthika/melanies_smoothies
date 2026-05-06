c56ffcc4af3e94077c870c4b71a38aeba62b9a7d
MaheshKarthika Gurusamy <152415375+gmkarthika@users.noreply.github.com>
2026-05-06 14:36:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,11 +56,11 @@ if ingredients_list:
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + ' '
         search_on = pd_df.loc[pd_df['FRUIT_NAME']  == fruits_chosen,  'SEARCH_ON'].iloc[0]
-        st.write('The srach value for', fruits_chosen, ' is ', search_on)
+        st.write('The search value for', fruits_chosen, ' is ', search_on)
       
         #Display smoothiefroot nutrition information
         st.subheader(fruits_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruits_chosen )  
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}" )  
         st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
 
     st.write(ingredients_string)
