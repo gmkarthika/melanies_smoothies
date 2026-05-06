c4ab69e948170e6d499e34851e87abe2644ee480
MaheshKarthika Gurusamy <152415375+gmkarthika@users.noreply.github.com>
2026-05-06 13:59:31 -0400

    Update streamlit_app.py
    Updated fruit_chosen in the url to get the fruit name selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ if ingredients_list:
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + ' '
         #Display smoothiefroot nutrition information
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruits_chosen )  
         st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
 
     st.write(ingredients_string)
